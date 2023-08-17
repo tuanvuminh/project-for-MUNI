ff9fc276962f71347c4aaafa30f0344381f90524
„Tuan <tuan38@seznam.czgit config --global user.name Tuan>
2023-08-17 22:19:55 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Porty
+        String serverIP = "78.128.211.58";
+        int serverPort = 4444;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        // Komunikace
+        try (Socket socket = new Socket(serverIP, serverPort);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter sendMessage = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader readMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Připojení k serveru proběhlo úspěšně. Pro ukončení konverzace zadejte \"exit\"");
+
+            String userMessage;
+            for (boolean continueConversation = true; continueConversation; ) {
+                userMessage = reader.readLine();
+
+                if (userMessage.equalsIgnoreCase("exit")) {
+                    continueConversation = false;
+                } else {
+                    sendMessage.println(userMessage);
+                    String serverResponse = readMessage.readLine();
+                    System.out.println("Vaše poslaná zpráva: " + serverResponse);
+                }
+            }
+            System.out.println("Byli jste odpojeni ze serveru.");
+        } catch (IOException e) {
+            System.err.println("Nastala chyba při připojení.");
         }
     }
 }
